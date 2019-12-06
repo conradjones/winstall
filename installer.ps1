e54970efa895a44b4cf1fa5325d9ac2e626210fb
Conrad Jones <cjones@turboit.co.uk>
2019-12-06 14:54:56 +0000

    add configurable parameters to installer script

--- installer.ps1
+++ installer.ps1
@@ -1,4 +1,5 @@
 param(  [Parameter(Mandatory=$true)][String]$ComponentPath,
+        $Parameters,
         [switch] $DetectOnly)
 
 $ErrorActionPreference = “Stop”
@@ -24,6 +25,18 @@ function Log ($RunFolder, [LogLevel]$LogLevel, $Line)
     }
 }
 
+function Get-ParsedNodeValue($Value, $Parameters)
+{
+    if ($null -eq $Value) {
+        return $null
+    }
+    foreach ($Key in $Parameters.Keys) {
+        $PlaceHolder = '${' + $($Key) + '}'
+        $Value = $Value.Replace($PlaceHolder, $Parameters[$Key])
+    }
+    return $Value
+}
+
 function Get-RunFolder($ComponentName)
 {
     $Folder = "C:\.winstall\install\$ComponentName"
@@ -34,23 +47,27 @@ function Get-RunFolder($ComponentName)
     return $Folder
 }
 
-function Step-Download($XmlNode, $RunFolder)
+function Step-Download($XmlNode, $RunFolder, $Parameters)
 {
-    $FileName = $XmlNode.InnerText  | Split-Path -Leaf
+    $Url = Get-ParsedNodeValue -Value $XmlNode.InnerText -Parameters $Parameters
+
+    $FileName = $Url | Split-Path -Leaf
     $Output = Join-Path -Path $RunFolder -ChildPath $FileName
     $WebClient = New-Object System.Net.WebClient
-    Log -RunFolder $RunFolder -LogLevel Info -Line "Downloading:$($XmlNode.InnerText)"
-    $WebClient.DownloadFile($XmlNode.InnerText, $Output)
+    Log -RunFolder $RunFolder -LogLevel Info -Line "Downloading:$Url)"
+    $WebClient.DownloadFile($Url, $Output)
+
     if (!(Test-Path -Path $Output)) {
-        Log -RunFolder $RunFolder -LogLevel Error -Line "Failed to download $($XmlNode.InnerText) to $Output"
+        Log -RunFolder $RunFolder -LogLevel Error -Line "Failed to download $Url to $Output"
         return $False
     }
+
     return $True
 }
 
-function Step-CreateDir($XmlNode, $RunFolder)
+function Step-CreateDir($XmlNode, $RunFolder, $Parameters)
 {
-    $FolderName = $XmlNode.InnerText
+    $FolderName = Get-ParsedNodeValue -Value $XmlNode.InnerText -Parameters $Parameters
     if (Test-Path -Path $FolderName) {
        if ((Get-Item $FolderName) -is [System.IO.DirectoryInfo]) {
            return $true
@@ -66,15 +83,15 @@ function Step-CreateDir($XmlNode, $RunFolder)
     return $True
 }
 
-function Step-CopyFile($XmlNode, $RunFolder)
+function Step-CopyFile($XmlNode, $RunFolder, $Parameters)
 {
-    $SourceNode = $XmlNode.source
+    $SourceNode = Get-ParsedNodeValue -Value $XmlNode.source -Parameters $Parameters
     if ($Null -eq $SourceNode) {
        Log -RunFolder $RunFolder -LogLevel Error -Line "source node is missing from copy_file step"
        return $False
     }
 
-    $DestNode = $XmlNode.dest
+    $DestNode = Get-ParsedNodeValue -Value $XmlNode.dest -Parameters $Parameters
     if ($Null -eq $DestNode) {
        Log -RunFolder $RunFolder -LogLevel Error -Line "dest node is missing from copy_file step"
        return $False
@@ -90,9 +107,41 @@ function Step-CopyFile($XmlNode, $RunFolder)
     return $True
 }
 
-function Step-Path($XmlNode, $RunFolder)
+
+function Step-ConfigureFile($XmlNode, $RunFolder, $Parameters)
 {
-    $PathToAdd = $XmlNode.InnerText
+    $SourceNode = Get-ParsedNodeValue -Value $XmlNode.source -Parameters $Parameters
+    if ($Null -eq $SourceNode) {
+       Log -RunFolder $RunFolder -LogLevel Error -Line "source node is missing from copy_file step"
+       return $False
+    }
+
+    $DestPath = Get-ParsedNodeValue -Value $XmlNode.dest -Parameters $Parameters
+    if ($Null -eq $DestPath) {
+       Log -RunFolder $RunFolder -LogLevel Error -Line "dest node is missing from copy_file step"
+       return $False
+    }
+
+    $SourcePath = Join-Path -Path $RunFolder -ChildPath $SourceNode
+
+    $AsText = Get-Content -Path $SourcePath
+    foreach ($Key in $Parameters.Keys) {
+        $PlaceHolder = '${' + $($Key) + '}'
+        $AsText = $AsText.Replace($PlaceHolder, $Parameters[$Key])
+    }
+
+    $AsText | Out-File -FilePath $DestPath
+    if (!(Test-Path -Path $DestPath)) {
+        Log -RunFolder $RunFolder -LogLevel Error -Line "Failed to configure file $SourcePath to $DestPath"
+        return $False
+    }
+    return $True
+}
+
+function Step-Path($XmlNode, $RunFolder, $Parameters)
+{
+    $PathToAdd = Get-ParsedNodeValue -Value $XmlNode.InnerText -Parameters $Parameters
+
     $PathKeyValue = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
     $AllPaths = $PathKeyValue.split(";")
     if ($AllPaths.Contains($PathToAdd)) {
@@ -110,13 +159,13 @@ function Step-Path($XmlNode, $RunFolder)
     return $True
 }
 
-function Step-Command($XmlNode, $RunFolder)
+function Step-Command($XmlNode, $RunFolder, $Parameters)
 {
     Push-Location -Path $RunFolder
-    $command = $XmlNode.commandLine
+    $command = Get-ParsedNodeValue -Value $XmlNode.commandLine -Parameters $Parameters
     $args = @()
     foreach ($ArgNode in $XmlNode.args.arg) {
-        $args += $ArgNode
+        $args += Get-ParsedNodeValue -Value $ArgNode -Parameters $Parameters
     }
     $argString = $args -join " "
     Log -RunFolder $RunFolder -LogLevel Info -Line "Executing:$command $argString"
@@ -144,13 +193,14 @@ function Step-Command($XmlNode, $RunFolder)
     return $False
 }
 
-function Step-RegSet($XmlNode, $RunFolder)
+function Step-RegSet($XmlNode, $RunFolder, $Parameters)
 {
-    $RegKey = $XmlNode.key
-    $RegValue = $XmlNode.value
-    $RegValueName = $XmlNode.value_name
+    $RegKey = Get-ParsedNodeValue -Value $XmlNode.key -Parameters $Parameters
+    $RegValue = Get-ParsedNodeValue -Value $XmlNode.value -Parameters $Parameters
+    $RegValueName = Get-ParsedNodeValue -Value $XmlNode.value_name -Parameters $Parameters
+    $RegValueType = Get-ParsedNodeValue -Value $XmlNode.value_type -Parameters $Parameters
 
-    switch ($XmlNode.value_type) {
+    switch ($RegValueType) {
         "REG_DWORD" { $Type = [Microsoft.Win32.RegistryValueKind]::DWord}
         "REG_SZ"    { $Type = [Microsoft.Win32.RegistryValueKind]::String}
         default     { return $false }
@@ -161,9 +211,10 @@ function Step-RegSet($XmlNode, $RunFolder)
     return $True
 }
 
-function Step-Powershell($XmlNode, $RunFolder)
+function Step-Powershell($XmlNode, $RunFolder, $Parameters)
 {
-    $scriptBlock = $XmlNode.scriptblock
+    $scriptBlock = Get-ParsedNodeValue -Value $XmlNode.scriptblock -Parameters $Parameters
+
     if ($null -eq $scriptBlock) {
         Log -RunFolder $RunFolder -LogLevel Error -Line "scriptblock node is missing from powershell step"
         exit 1
@@ -174,15 +225,15 @@ function Step-Powershell($XmlNode, $RunFolder)
     return $True
 }
 
-function Step-Unzip($XmlNode, $RunFolder)
+function Step-Unzip($XmlNode, $RunFolder, $Parameters)
 {
-    $zipfile = $XmlNode.zipfile
+    $zipfile = Get-ParsedNodeValue -Value $XmlNode.zipfile -Parameters $Parameters
     if ($null -eq $zipfile) {
         Log -RunFolder $RunFolder -LogLevel Error -Line "zipfile node is missing from unzip step"
         exit 1
     }
 
-    $destination = $XmlNode.destination
+    $destination = Get-ParsedNodeValue -Value $XmlNode.destination -Parameters $Parameters
     if ($null -eq $destination) {
         Log -RunFolder $RunFolder -LogLevel Error -Line "destination node is missing from unzip step"
         exit 1
@@ -193,60 +244,69 @@ function Step-Unzip($XmlNode, $RunFolder)
     return $True
 }
 
-function Step-KillProcess($XmlNode, $RunFolder)
+function Step-KillProcess($XmlNode, $RunFolder, $Parameters)
 {
-    Get-Process -Name $XmlNode.InnerText | Stop-Process -Force -Verbose
+    $process_name = Get-ParsedNodeValue -Value $XmlNode.InnerText -Parameters $Parameters
+    Get-Process -Name $process_name | Stop-Process -Force -Verbose
 
     return $True
 }
 
-function Step-WaitProcess($XmlNode, $RunFolder)
+function Step-WaitProcess($XmlNode, $RunFolder, $Parameters)
 {
-    $Process = @(Get-Process -Name $XmlNode.InnerText -ErrorAction SilentlyContinue)
+    $process_name = Get-ParsedNodeValue -Value $XmlNode.InnerText -Parameters $Parameters
+    $Process = @(Get-Process -Name $process_name -ErrorAction SilentlyContinue)
 
     while ($Process.Count -gt 0) {
 
         Log -RunFolder $RunFolder -LogLevel Info -Line "Waiting for process:$XmlNode"
         Start-Sleep -Seconds 1
-        $Process = @(Get-Process -Name $XmlNode.InnerText -ErrorAction SilentlyContinue)
+        $Process = @(Get-Process -Name $process_name -ErrorAction SilentlyContinue)
     }
 
     return $True
 }
 
-function Confirm-FileDetected($DetectionNode, $RunFolder)
+function Confirm-FileDetected($DetectionNode, $RunFolder, $Parameters)
 {
-    if (!(Test-Path -Path $DetectionNode.path)) {
-        Log -RunFolder $RunFolder -LogLevel Info -Line "(Detection):$($DetectionNode.path) not found"
+    $path = Get-ParsedNodeValue -Value $DetectionNode.path -Parameters $Parameters
+
+    Log -RunFolder $RunFolder -LogLevel Info -Line "(Detection):checking existence of $path"
+    if (!(Test-Path -Path $path)) {
+        Log -RunFolder $RunFolder -LogLevel Info -Line "(Detection):$path not found"
         return $False
     }
 
-    if (!((Get-Item $DetectionNode.path) -is [System.IO.FileInfo])) {
-        Log -RunFolder $RunFolder -LogLevel Info -Line "(Detection):$($DetectionNode.path) found but is not a file"
+    Log -RunFolder $RunFolder -LogLevel Info -Line "(Detection):checking $path is a file"
+    if (!((Get-Item $path) -is [System.IO.FileInfo])) {
+        Log -RunFolder $RunFolder -LogLevel Info -Line "(Detection):$path found but is not a file"
         return $False
     }
 
-    $expectedVersion = $DetectionNode.version
+
+    $expectedVersion = Get-ParsedNodeValue -Value $DetectionNode.version -Parameters $Parameters
     if ($null -ne $expectedVersion) {
-        $foundVersion = (Get-Command $DetectionNode.path).Version
+        Log -RunFolder $RunFolder -LogLevel Info -Line "(Detection):checking $path is version $expectedVersion"
+        $foundVersion = (Get-Command $path).Version
         if ($foundVersion -ne [version]$expectedVersion) {
-            Log -RunFolder $RunFolder -LogLevel Info -Line "(Detection):$($DetectionNode.path) found but version is:$foundVersion (expected:$expectedVersion)"
+            Log -RunFolder $RunFolder -LogLevel Info -Line "(Detection):$path found but version is:$foundVersion (expected:$expectedVersion)"
             return $False
         }
     }
 
+    Log -RunFolder $RunFolder -LogLevel Info -Line "(Detection):$path is detected"
     return $True
 }
 
-function Confirm-PowershellDetected($DetectionNode, $RunFolder)
+function Confirm-PowershellDetected($DetectionNode, $RunFolder, $Parameters)
 {
-    $scriptBlock = $DetectionNode.scriptblock
+    $scriptBlock = Get-ParsedNodeValue -Value $DetectionNode.scriptblock -Parameters $Parameters
     if ($null -eq $scriptBlock) {
         Log -RunFolder $RunFolder -LogLevel Error -Line "Powershell detection node missing scriptblock"
         exit 1
     }
 
-    $expected = $DetectionNode.expected
+    $expected = Get-ParsedNodeValue -Value $DetectionNode.expected -Parameters $Parameters
     if ($null -eq $expected) {
         Log -RunFolder $RunFolder -LogLevel Error -Line "Powershell detection node missing expected"
         exit 1
@@ -254,18 +314,24 @@ function Confirm-PowershellDetected($DetectionNode, $RunFolder)
 
     $Result = Invoke-Expression -Command $scriptBlock
 
-    return ($Result -eq $expected)
+    $success = ($Result -eq $expected)
+    if (!$success)
+    {
+        Log -RunFolder $RunFolder -LogLevel Info -Line "Powershell detection rule failed expected:$expected got:$result`n$scriptBlock"
+    }
+    return $success
 }
 
-function Confirm-IsDetected($XmlNode, $RunFolder)
+function Confirm-IsDetected($XmlNode, $RunFolder, $Parameters)
 {
     foreach ($DetectionNode in $XmlNode.ChildNodes) {
         switch ($DetectionNode.LocalName) {
-            "file"          { if (!(Confirm-FileDetected        -DetectionNode $DetectionNode -RunFolder $RunFolder)) {return $false} ; break}
-            "powershell"    { if (!(Confirm-PowershellDetected  -DetectionNode $DetectionNode -RunFolder $RunFolder)) {return $false} ; break}
+            "file"          { if (!(Confirm-FileDetected        -DetectionNode $DetectionNode -RunFolder $RunFolder -Parameters $Parameters)) {return $false} ; break}
+            "powershell"    { if (!(Confirm-PowershellDetected  -DetectionNode $DetectionNode -RunFolder $RunFolder -Parameters $Parameters)) {return $false} ; break}
             default  {Log -RunFolder $RunFolder -LogLevel Warn -Line "Unknown detection step in XML $($DetectionNode.LocalName)"; exit 1}
         }
     }
+    Log -RunFolder $RunFolder -LogLevel Info -Line "(Detection):Confirm-IsDetected returning true"
     return $True
 }
 
@@ -286,7 +352,7 @@ function Copy-FolderContents($SourcePath, $DestinationPath)
     }
 }
 
-function Install-Component($ComponentPath, $DetectOnly)
+function Install-Component($ComponentPath, $DetectOnly, $Parameters)
 {
     $ComponentName = Split-Path -Path $ComponentPath -Leaf
 
@@ -314,9 +380,21 @@ function Install-Component($ComponentPath, $DetectOnly)
     $RunFolder = Get-RunFolder -ComponentName $ComponentName
     Log -RunFolder $RunFolder -LogLevel Info  -Line "Package:$ComponentName runfolder:$RunFolder"
 
+
+    $ParametersNode = $PackageNode.parameters
+    if ($null -ne $ParametersNode) {
+        foreach ($ParameterNode in $ParametersNode.ChildNodes) {
+            if (!($Parameters.ContainsKey($ParameterNode.LocalName)))
+            {
+                $Parameters[$ParameterNode.LocalName] = $ParameterNode.InnerText
+            }
+        }
+    }
+
+
     $DetectionNode = $PackageNode.detect
     if ($null -ne $DetectionNode ) {
-        if (Confirm-IsDetected -XmlNode $DetectionNode -RunFolder $RunFolder) {
+        if (Confirm-IsDetected -XmlNode $DetectionNode -RunFolder $RunFolder -Parameters $Parameters) {
             Log -RunFolder $RunFolder -LogLevel Info  -Line "Package is detected:$ComponentName"
             return "AlreadyInstalled"
         }
@@ -339,16 +417,17 @@ function Install-Component($ComponentPath, $DetectOnly)
 
     foreach ($StepNode in $PackageNode.steps.ChildNodes) {
         switch ($StepNode.LocalName) {
-            "download"     { if (!(Step-Download    -XmlNode $StepNode -RunFolder $RunFolder)) {return "Fail"} ; break}
-            "create_dir"   { if (!(Step-CreateDir   -XmlNode $StepNode -RunFolder $RunFolder)) {return "Fail"} ; break}
-            "copy_file"    { if (!(Step-CopyFile    -XmlNode $StepNode -RunFolder $RunFolder)) {return "Fail"} ; break}
-            "path"         { if (!(Step-Path        -XmlNode $StepNode -RunFolder $RunFolder)) {return "Fail"} ; break}
-            "command"      { if (!(Step-Command     -XmlNode $StepNode -RunFolder $RunFolder)) {return "Fail"} ; break}
-            "reg_set"      { if (!(Step-RegSet      -XmlNode $StepNode -RunFolder $RunFolder)) {return "Fail"} ; break}
-            "powershell"   { if (!(Step-Powershell  -XmlNode $StepNode -RunFolder $RunFolder)) {return "Fail"} ; break}
-            "unzip"        { if (!(Step-Unzip       -XmlNode $StepNode -RunFolder $RunFolder)) {return "Fail"} ; break}
-            "kill_process" { if (!(Step-KillProcess -XmlNode $StepNode -RunFolder $RunFolder)) {return "Fail"} ; break}
-            "wait_process" { if (!(Step-WaitProcess -XmlNode $StepNode -RunFolder $RunFolder)) {return "Fail"} ; break}
+            "download"       { if (!(Step-Download      -XmlNode $StepNode -RunFolder $RunFolder -Parameters $Parameters )) {return "Fail"} ; break}
+            "create_dir"     { if (!(Step-CreateDir     -XmlNode $StepNode -RunFolder $RunFolder -Parameters $Parameters )) {return "Fail"} ; break}
+            "copy_file"      { if (!(Step-CopyFile      -XmlNode $StepNode -RunFolder $RunFolder -Parameters $Parameters )) {return "Fail"} ; break}
+            "configure_file" { if (!(Step-ConfigureFile -XmlNode $StepNode -RunFolder $RunFolder -Parameters $Parameters )) {return "Fail"} ; break}
+            "path"           { if (!(Step-Path          -XmlNode $StepNode -RunFolder $RunFolder -Parameters $Parameters )) {return "Fail"} ; break}
+            "command"        { if (!(Step-Command       -XmlNode $StepNode -RunFolder $RunFolder -Parameters $Parameters )) {return "Fail"} ; break}
+            "reg_set"        { if (!(Step-RegSet        -XmlNode $StepNode -RunFolder $RunFolder -Parameters $Parameters )) {return "Fail"} ; break}
+            "powershell"     { if (!(Step-Powershell    -XmlNode $StepNode -RunFolder $RunFolder -Parameters $Parameters )) {return "Fail"} ; break}
+            "unzip"          { if (!(Step-Unzip         -XmlNode $StepNode -RunFolder $RunFolder -Parameters $Parameters )) {return "Fail"} ; break}
+            "kill_process"   { if (!(Step-KillProcess   -XmlNode $StepNode -RunFolder $RunFolder -Parameters $Parameters )) {return "Fail"} ; break}
+            "wait_process"   { if (!(Step-WaitProcess   -XmlNode $StepNode -RunFolder $RunFolder -Parameters $Parameters )) {return "Fail"} ; break}
             default {Log -RunFolder $RunFolder -LogLevel Error -Line "Unknown step in XML $($StepNode.LocalName)"; break}
         }
     }
@@ -360,7 +439,7 @@ function Install-Component($ComponentPath, $DetectOnly)
     }
 
     if ($null -ne $DetectionNode ) {
-        if (!(Confirm-IsDetected -XmlNode $DetectionNode -RunFolder $RunFolder)) {
+        if (!(Confirm-IsDetected -XmlNode $DetectionNode -RunFolder $RunFolder -Parameters $Parameters)) {
             Log -RunFolder $RunFolder -LogLevel Error  -Line "Package is not detected after install $ComponentName"
             return "Fail"
         }
@@ -371,4 +450,4 @@ function Install-Component($ComponentPath, $DetectOnly)
 }
 
 
-Install-Component -ComponentPath $ComponentPath -DetectOnly:$DetectOnly
+Install-Component -ComponentPath $ComponentPath -DetectOnly:$DetectOnly -Parameters $Parameters
